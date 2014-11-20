7081741abeae95f2ede5f3f120e0d1332812655d
Chris Lee <chl@umich.edu>
2014-11-20 12:29:48 -0500

    Updated imagePathFactory

--- src/factory/ImagePathFactory.java
+++ src/factory/ImagePathFactory.java
@@ -8,32 +8,37 @@ import exceptions.ImagePathException;
 public class ImagePathFactory
 {
   /**
-   * Creates a path to a vehicle or parcel image.
+   * Creates a path to a pokemon image
    *
    * @param type
    *     The type of image you want to find the path for.
    *
    * @return The path of the image.
    *
-   * @throws eecs285.proj3.colindj.Exceptions.ImagePathException
+   * @throws exceptions.ImagePathException
    *     Occurs when the type isn't recognized.
    */
   public static String createImagePath( String name ) throws ImagePathException
   {
     switch ( name )
     {
+      
+      case "Bulbasaur":
+        return "images/bulbasaur.png";
+      case "Charmander":
+        return "images/charmander.png";
+      case "Squirtle":
+        return "images/squirtle.png";
+      case "Pikachu":
+        return "images/pikachu.png";
+      case "Caterpie":
+        return "images/caterpie.png";
+      case "Pidgey":
+        return "images/pidgey.png";
       case "Rattata":
         return "images/rattata.png";
-      case "Freight Truck":
-        return "images/Freight_Truck.jpg";
-      case "Cargo Plane":
-        return "images/Cargo_Plane.jpg";
-      case "Large":
-        return "images/Large_Parcel.png";
-      case "Medium":
-        return "images/Medium_Parcel.png";
-      case "Small":
-        return "images/Small_Parcel.png";
+      case "Geodude":
+        return "images/geodude.png";
       default:
         throw new ImagePathException();
     }
