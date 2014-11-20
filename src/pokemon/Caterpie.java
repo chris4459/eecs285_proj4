99633592f135bf5d86fe1b561a31b9b6e10abf49
Chris Lee <chl@umich.edu>
2014-11-19 20:38:36 -0500

    Added additional pokemons and images

---
+++ src/pokemon/Caterpie.java
@@ -0,0 +1,7 @@
+package pokemon;
+
+public class Caterpie extends Pokemon implements Cloneable {
+  public Caterpie() {
+    super("Caterpie", 45, 20, 20, 45, 3);
+  }
+}
---
+++ src/pokemon/Geodude.java
@@ -0,0 +1,7 @@
+package pokemon;
+
+public class Geodude extends Pokemon implements Cloneable{
+  public Geodude() {
+    super("Geodude", 40, 30, 30, 20, 5);
+  }
+}
